46aa89ab0a2ed13c82b2e2eff99369fcb50368b5
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2023-04-22 21:44:08 -0600

    [TlahuicoleSystem]feature-User_Management

--- src/database/queries.js
+++ src/database/queries.js
@@ -1,6 +1,7 @@
 export const queries = {
     consultarc: 'SELECT id,nombre,tcompania_id,trol_id, tusuario_admin_id, tusuario_conductor_id, tusuario_pasajero_id FROM tusuario WHERE usuario = ? AND contraseña = ? AND estado = 1',
     consultarAll: 'SELECT * FROM tusuario WHERE tcompania_id = ? AND estado = 1',
+    consultarUsuario: 'SELECT nombre, primer_apellido, segundo_apellido, trol_id, tusuario_admin_id, tusuario_conductor_id, tusuario_pasajero_id, usuario, contraseña FROM tusuario WHERE id = ?  AND estado = 1',
     consultarA: 'SELECT * FROM tusuario_admin WHERE id = ? AND estado = 1',
     consultarC: 'SELECT * FROM tusuario_conductor WHERE id = ? AND estado = 1',
     consultarP: 'SELECT * FROM tusuario_pasajero WHERE id = ? AND estado = 1',
@@ -9,6 +10,10 @@ export const queries = {
     insertarPasajero: 'INSERT INTO tusuario_pasajero set ?',
     insertarUsuario: 'INSERT INTO tusuario set ?',
     eliminarUsuario: 'UPDATE tusuario SET estado = 0 WHERE id = ?',
+    actualizarA: 'UPDATE tusuario_admin set ? WHERE id = ?',
+    actualizarC: 'UPDATE tusuario_conductor set ? WHERE id = ?',
+    actualizarP: 'UPDATE tusuario_pasajero set ? WHERE id = ?',
+    actualizarUsuario: 'UPDATE tusuario set ? WHERE id = ?',
     //favorits: 'SELECT pedidos.codigoBarras, productos.foto, productos.nombre, productos.descripcion_breve, productos.precio, COUNT( pedidos.codigoBarras ) AS total FROM pedidos INNER JOIN productos ON pedidos.codigoBarras = productos.codigo_barras GROUP BY pedidos.codigoBarras ORDER BY total DESC',
     //buscar: 'SELECT codigo_barras, foto, nombre, descripcion_breve, precio FROM productos WHERE nombre LIKE "%"?"%" ',
 }
--- src/servicios/productos.servicio.js
+++ src/servicios/productos.servicio.js
@@ -41,6 +41,34 @@ export const consultarUsuariosService = async(compañia) => {
     return respuesta
 }
 
+export const consultarUsuarioService = async(id) => {
+    let respuesta = null
+    let complemento = null
+    let obj_unidos = null
+    try {
+        const conn = await getConnetion()
+        respuesta = await conn.query(queries.consultarUsuario, id)
+        switch (respuesta[0].trol_id) {
+            case 1:
+                complemento = await conn.query(queries.consultarA, [respuesta[0].tusuario_admin_id])
+                break;
+            case 2:
+                complemento = await conn.query(queries.consultarC, [respuesta[0].tusuario_conductor_id])
+                break;
+            case 3:
+                complemento = await conn.query(queries.consultarP, [respuesta[0].tusuario_pasajero_id])
+                break;
+            default:
+                respuesta = "rol no encontrado"
+                break;
+        }
+    } catch (e) {
+        throw e.message
+    }
+    obj_unidos = Object.assign(respuesta[0],complemento[0]);
+    return obj_unidos
+}
+
 export const insertarAdminService = async(admin) => {
     let idNewProduct = null
     try {
@@ -93,6 +121,41 @@ export const eliminarUsuarioService = async (id) => {
     try {
         const conn = await getConnetion()
         await conn.query(queries.eliminarUsuario, id)
+    } catch (e) {
+        throw e.message
+    }
+}
+
+export const actualizarAdminService = async(administrador, id) => {
+    try {
+        const conn = await getConnetion()
+        await conn.query(queries.actualizarA, [administrador, id])
+    } catch (e) {
+        throw e.message
+    }
+}
+
+export const actualizarCondcutorService = async(conductor, id) => {
+    try {
+        const conn = await getConnetion()
+        await conn.query(queries.actualizarC, [conductor, id])
+    } catch (e) {
+        throw e.message
+    }
+}
+export const actualizarPasajeroService = async(pasajero, id) => {
+    try {
+        const conn = await getConnetion()
+        await conn.query(queries.actualizarP, [pasajero, id])
+    } catch (e) {
+        throw e.message
+    }
+}
+
+export const actualizarUsuarioService = async(usuario, id) => {
+    try {
+        const conn = await getConnetion()
+        await conn.query(queries.actualizarUsuario, [usuario, id])
     } catch (e) {
         throw e.message
     }
